5d7cc6870813f01bfed1a6631214e4988f5f0529
Olendroza <34488056+Olendroza@users.noreply.github.com>
2019-01-23 12:07:10 +0100

    Updates

---
+++ precache-manifest.cfa1488bab21d6bacd07e53d533e3150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f82d6aad7842ebbc78a",
+    "url": "/strona-do-wydarzen/static/js/runtime~main.8f82d6aa.js"
+  },
+  {
+    "revision": "47b35c516effbf7c2487",
+    "url": "/strona-do-wydarzen/static/js/main.47b35c51.chunk.js"
+  },
+  {
+    "revision": "19a2291b5993f890d205",
+    "url": "/strona-do-wydarzen/static/js/1.19a2291b.chunk.js"
+  },
+  {
+    "revision": "47b35c516effbf7c2487",
+    "url": "/strona-do-wydarzen/static/css/main.743aac1c.chunk.css"
+  },
+  {
+    "revision": "19a2291b5993f890d205",
+    "url": "/strona-do-wydarzen/static/css/1.7569ec05.chunk.css"
+  },
+  {
+    "revision": "85920483f0378860920f77d0ac1af89d",
+    "url": "/strona-do-wydarzen/index.html"
+  }
+];
